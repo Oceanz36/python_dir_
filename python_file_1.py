89922bd7829c49952ba663fa17bd46c587f59d41
Oceanz36 <phillip.semakula@yahoo.com>
2025-10-10 18:37:52 -0400

    staged 2 python files

---
+++ python_file_1.py
@@ -0,0 +1,12 @@
+Days_of_the_week = ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
+
+temp = 61
+
+if temp > 60:
+   print('It is a hot day')
+
+elif temp < 30:
+    print('It is freezing')
+
+else:
+    print('Watch the weather forecast')
