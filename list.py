f8a1368ba528cd59d017a4943c71117939b53bbc
Oceanz36 <phillip.semakula@yahoo.com>
2025-10-15 20:48:52 -0400

    Added 3 python files

---
+++ list.py
@@ -0,0 +1,47 @@
+x = [1, 2, 3, 4, 5, 6]
+print(x)
+
+for item in x:
+    print(x)
+
+x.append(7)
+print(x)
+
+x.remove(1)
+print (x)
+
+for number in x:
+    print(x)
+
+    y = range(7)
+    print(y)
+
+    u= [1, 2, 3, 4, 5]
+    u.insert(2, 9)
+    print(u)
+
+    u.remove(9)
+    print(u)
+
+    u.append(9)
+    print(u)
+
+    t= [1, 2, 3, 4, 5]
+    for x in t:
+        print(x)
+
+        print(len(t))
+
+        t= [1, 2, 3, 4, 5,6]
+        print(t[0:4])
+        print(3 in t)
+        print(len(t))
+
+        p = range(10)
+        print(p)
+        for p in p:
+            print (p)
+
+
+
+    
